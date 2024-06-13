fb8cccbfc908b79f144abe868aff322f5fe3fc7d
bgpkrishna123 <bgpkrishna123@gmail.com>
2024-06-13 11:47:47 +0530

    backend

---
+++ Backend/src/controlles/event.js
@@ -0,0 +1,136 @@
+const { EventModel } = require("../models/event.schema");
+const { UserModel } = require("../models/user.schema");
+
+const getEvents = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 20;
+        const { minPrice, maxPrice, city, sort, search } = req.query;
+        const query = {};
+
+        if (minPrice && maxPrice) {
+            query.Price = { $gte: minPrice, $lte: maxPrice };
+        } else if (minPrice) {
+            query.Price = { $gte: minPrice };
+        } else if (maxPrice) {
+            query.Price = { $lte: maxPrice };
+        }
+
+        if (city) {
+            query.location = city;
+        }
+
+        if (search) {
+            query.$or = [
+                { title: { $regex: search, $options: "i" } },
+                { category: { $regex: search, $options: "i" } },
+                { eventPlaner: { $regex: search, $options: "i" } },
+                { organizer: { $regex: search, $options: "i" } },
+            ];
+        }
+
+        const totalCount = await EventModel.countDocuments(query);
+        const totalPages = Math.ceil(totalCount / limit);
+
+        const sortCriteria = sort === "desc" ? { eventDate: -1 } : { eventDate: 1 };
+
+        const skip = (page - 1) * limit;
+        const events = await EventModel.find(query).skip(skip).sort(sortCriteria).limit(limit);
+
+        res.status(200).json({ events, totalPages });
+    } catch (err) {
+        console.error("Error while filtering, searching, and paginating events:", err);
+        res.status(500).json({ error: err.message || "Internal Server Error" });
+    }
+};
+
+const eventData = async (req, res) => {
+    const userID = req.userID;
+    try {
+        const user = await UserModel.findById(userID).populate('eventsBooked');
+        if (!user) {
+            return res.status(404).json({ error: true, message: "User not found" });
+        }
+
+        const eventsArray = user.eventsBooked;
+        if (!eventsArray || eventsArray.length === 0) {
+            return res.status(200).json({ error: false, message: "User has no booked events", item: [] });
+        }
+
+        res.status(200).json({ error: false, item: eventsArray });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: true, message: "Internal Server Error" });
+    }
+};
+
+const event = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const eventData = await EventModel.findById(id);
+        if (!eventData) {
+            return res.status(404).json({ error: true, message: "Event not found" });
+        }
+        res.status(200).json({ error: false, item: eventData });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ error: true, message: error.message });
+    }
+};
+
+
+const addEvent = async (req, res) => {
+    try {
+        const { title, description, eventDate, category, imageUrl, mode, time, eventPlaner, Price, location, ticketTypes } = req.body;
+
+        const organizer = req.username; 
+        const newEvent = new EventModel({ title, description, eventDate, mode, time, organizer, category, imageUrl, eventPlaner, Price, location, ticketTypes });
+        
+        const savedEvent = await newEvent.save();
+
+        await UserModel.findOneAndUpdate({ username: eventPlaner }, { $push: { eventsPlanned: savedEvent._id } }, { new: true });
+        await UserModel.findByIdAndUpdate(req.userID, { $push: { eventsBooked: savedEvent._id } }, { new: true });
+
+        res.status(201).json({ success: true, event: savedEvent });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Not able to add Event" });
+    }
+};
+
+const updateEvent = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updatedEvent = await EventModel.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedEvent) {
+            return res.status(404).json({ success: false, message: "Event not found" });
+        }
+        res.status(200).json({ success: true, event: updatedEvent });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+};
+
+const deleteEvent = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const usersToUpdate = await UserModel.find({ eventsPlanned: id });
+        for (const user of usersToUpdate) {
+            user.eventsPlanned = user.eventsPlanned.filter(eventId => eventId.toString() !== id);
+            await user.save();
+        }
+
+        const deletedEvent = await EventModel.findByIdAndDelete(id);
+        if (!deletedEvent) {
+            return res.status(404).json({ success: false, message: "Event not found" });
+        }
+
+        res.status(200).json({ success: true, message: "Event deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+};
+
+module.exports = { getEvents, eventData, event, addEvent, updateEvent, deleteEvent };
---
+++ Backend/src/models/event.schema.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const categoryEnum = [
+  "Music",
+  "Tech",
+  "Workshop",
+  "Sports",
+  "Photography",
+  "Food",
+  "Art",
+  "Expo",
+  "Fashion",
+  "Education",
+  "Health",
+  "Business",
+  "Science",
+  "Travel",
+  "Entertainment",
+  "Fitness",
+  "Culture",
+  "Environment",
+  "Politics",
+  "Religion",
+  "History",
+];
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    eventDate: { type: Date, required: true },
+    organizer: { type: String },
+    category: { type: String, enum: categoryEnum, required: true },
+    imageUrl: { type: String },
+    eventPlaner:{type:String},
+    time:{type: String},
+    mode:{type:String,required:true},
+    Price:{type:Number,required:true},
+    location: { type: String, required: true },
+    ticketTypes: {
+    type: [{ type: String, enum:  ["Gold", "Silver", "Bronze"] }],
+      default: function () {
+          return ["Bronze"];
+      }
+  },
+},
+  { versionKey: false }
+);
+
+const EventModel = mongoose.model("event", EventSchema);
+
+module.exports = { EventModel };
