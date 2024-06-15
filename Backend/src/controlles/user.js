b0998e395768d52346a91dae6d7d1f8e6540e214
bgpkrishna123 <bgpkrishna123@gmail.com>
2024-06-15 15:44:09 +0530

    routes change

--- Backend/src/controlles/user.js
+++ Backend/src/controlles/user.js
@@ -5,6 +5,23 @@ const { Blacklist } = require("../models/blacklist.Schema");
 
 require("dotenv").config();
 
+const getUserById = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ msg: "User not found." });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return res.status(500).json({ msg: "Failed to fetch user." });
+  }
+};
+
+
 const SignUp = async (req, res) => {
   const { username, email, role, password, profilePicture, eventsBooked } = req.body;
 
@@ -93,4 +110,4 @@ const logout = async (req, res) => {
   }
 };
 
-module.exports = { SignUp, logIn, forgotPassword, logout };
+module.exports = { SignUp, logIn,getUserById, forgotPassword, logout };
--- Backend/src/routes/event.routes.js
+++ Backend/src/routes/event.routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const access = require('../Middleware/access');
 const auth = require('../Middleware/auth');
-const { getEvents, eventData, addEvent, updateEvent, deleteEvent, event } = require('../controlles/event');
+const { getEvents, addEvent, updateEvent, deleteEvent, event } = require('../controlles/event');
 
 const eventRouter = express.Router();
 
 eventRouter.get('/', getEvents);
 eventRouter.get('/:id', event);
 
-eventRouter.get('/user/events',auth, eventData);
+//eventRouter.get('/user/events',auth, eventData);
 
 eventRouter.post('/',auth, access('eventPlanner', 'admin'), addEvent);
 eventRouter.patch('/:id',auth, access('eventPlanner', 'admin'), updateEvent);
